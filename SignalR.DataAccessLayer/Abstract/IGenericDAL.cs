14ec24e7d65c18e2f2f1189f89b97f072fef02cb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-08 13:56:44 +0300

    added interfaces

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetByID(int id);
+        List<T> GetListAll();
+    }
+}
