f43c67cd1184f822cc6263353ad3f4b9ff9530b1
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-11 01:04:20 +0300

    About API Controller

--- SignalRProject.API/Program.cs
+++ SignalRProject.API/Program.cs
@@ -1,6 +1,17 @@
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<Context>();
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDAL, EFAboutDAL>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
