d49fab22584d4ef0cde7904747bdcd01e4a2bc9b
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-11 17:11:03 +0300

    Admin Layout

---
+++ SignalRProject.UI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.UI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRProject.UI/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRProject.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
