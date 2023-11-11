de639bbb08ebf0a6530fd325281a4561483d5aeb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-11 18:04:43 +0300

    Admin - Category API Consume

---
+++ SignalRProject.UI/DTOs/CategoryDTOs/CreateCategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRProject.UI.DTOs.CategoryDTOs
+{
+	public class CreateCategoryDTO
+	{
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
---
+++ SignalRProject.UI/DTOs/CategoryDTOs/ResultCategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRProject.UI.DTOs.CategoryDTOs
+{
+	public class ResultCategoryDTO
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
