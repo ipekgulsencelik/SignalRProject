625370e66c9b98a6ab88372acce48a70e0509b03
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-14 22:41:02 +0300

    Discount İşlemleri

---
+++ SignalRProject.UI/DTOs/DiscountDTOs/CreateDiscountDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRProject.UI.DTOs.DiscountDTOs
+{
+	public class CreateDiscountDTO
+	{
+		public string Title { get; set; }
+		public string Amount { get; set; }
+		public string Description { get; set; }
+		public string ImageURL { get; set; }
+	}
+}
---
+++ SignalRProject.UI/DTOs/DiscountDTOs/ResultDiscountDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRProject.UI.DTOs.DiscountDTOs
+{
+	public class ResultDiscountDTO
+	{
+		public int DiscountID { get; set; }
+		public string Title { get; set; }
+		public string Amount { get; set; }
+		public string Description { get; set; }
+		public string ImageURL { get; set; }
+	}
+}
