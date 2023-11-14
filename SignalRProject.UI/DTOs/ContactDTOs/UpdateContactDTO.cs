4b1d735dfc43897f8fd0b23d138576ef96ff9684
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-14 15:05:30 +0300

    Contact İşlemleri

---
+++ SignalRProject.UI/DTOs/ContactDTOs/UpdateContactDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRProject.UI.DTOs.ContactDTOs
+{
+    public class UpdateContactDTO
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+    }
+}
